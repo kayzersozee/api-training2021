bad9184508e60403b926792ba027cd6192012063
kayzersozee <63726574+kayzersozee@users.noreply.github.com>
2021-01-28 21:55:02 +0100

    api data started

---
+++ src/data.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const URL = "https://movies-tvshows-data-imdb.p.rapidapi.com/";
+export const FILTER = "?type=get-movies-byyear";
+export const MOVIE_YEAR = "&page=1&year=2020";
